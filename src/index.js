15c52b370e881e3843c0ed27f990b4dfc2287cba
Zavadskaya Olga <zavadskaya.ov@gmail.com>
2023-01-23 22:56:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function toReadable (number) {
+  let arrUnits = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  let arrTeens = ['ten','eleven','twelve','thirteen', 'fourteen', 'fifteen', 'sixteen','seventeen','eighteen','nineteen'];
+  let arrDozens = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+  let humanNumber = '';
+  let temp = 0;
+
+  temp = Math.trunc(number/100); 
+  if (temp > 0 ) { 
+    humanNumber = arrUnits[temp] + ' hundred';
+  }
+
+  temp = number%100; 
+  if (temp > 19 && temp < 100) {
+    if (number > 100) {
+      humanNumber += ' ';
+    }
+    humanNumber += arrDozens[Math.trunc(temp/10)]; 
+    
+    if (temp%10 != 0) {
+      humanNumber += ' ' + arrUnits[temp%10];
+    }
+  }
+
+  if (temp > 9 && temp < 20) { 
+    if (number > 100) {
+      humanNumber += ' '; 
+    }
+    humanNumber += arrTeens[temp%10];            
+  }
   
+  if (temp < 10)
+  {
+    if (number > 10 && temp > 0) {
+      humanNumber +=' ' + arrUnits[temp%10];     
+    }
+    if (number < 10 ) {
+      humanNumber = arrUnits[temp%10];   
+    }
+  }
+    
+  return humanNumber;
 }
