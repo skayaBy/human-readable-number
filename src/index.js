bffb6081fa39748deb576863fe585e398e099c29
Zavadskaya Olga <zavadskaya.ov@gmail.com>
2023-01-24 09:50:59 +0300

    refactor: change solution

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 module.exports = function toReadable (number) {
-  let arrUnits = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  let arrUnits = ['','one','two','three','four','five','six','seven','eight','nine'];
   let arrTeens = ['ten','eleven','twelve','thirteen', 'fourteen', 'fifteen', 'sixteen','seventeen','eighteen','nineteen'];
-  let arrDozens = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+  let arrDozens = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
   let humanNumber = '';
   let temp = 0;
+
+  if (number == 0) humanNumber = 'zero';
 
   temp = Math.trunc(number/100); 
   if (temp > 0 ) { 
     humanNumber = arrUnits[temp] + ' hundred';
   }
-
-  temp = number%100; 
-  if (temp > 19 && temp < 100) {
+  
+  temp = number%100;  
+  if (temp > 19) {
     if (number > 100) {
       humanNumber += ' ';
     }
     humanNumber += arrDozens[Math.trunc(temp/10)]; 
-    
-    if (temp%10 != 0) {
-      humanNumber += ' ' + arrUnits[temp%10];
+    if (temp%10 > 0) {
+      humanNumber += ' ' + arrUnits[temp%10]; 
     }
   }
 
@@ -28,16 +29,13 @@ module.exports = function toReadable (number) {
     }
     humanNumber += arrTeens[temp%10];            
   }
-  
-  if (temp < 10)
-  {
-    if (number > 10 && temp > 0) {
-      humanNumber +=' ' + arrUnits[temp%10];     
+
+  if (temp <= 9) {
+    if (number > 100 && temp != 0) {
+      humanNumber += ' ';
     }
-    if (number < 10 ) {
-      humanNumber = arrUnits[temp%10];   
-    }
+    humanNumber += arrUnits[temp]; 
   }
-    
+  
   return humanNumber;
 }
